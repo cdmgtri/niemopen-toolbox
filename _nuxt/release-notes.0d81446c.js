e83dd99c17aad67e2221476bf66efa50d2ef7c0d
CDM <cdmgtri@users.noreply.github.com>
2024-03-08 18:26:05 -0500

    Updates

--- _nuxt/release-notes.0d81446c.js
+++
@@ -1,5 +0,0 @@
-import{_ as s}from"./page-header.529b67f0.js";import{o,b as t,e as a,f as n,u as r}from"./entry.cf2a792b.js";import"./pages.076ab20e.js";const _=`<h2 id="v01020230331">v0.1.0 (2023-03-31)</h2>
-<h3 id="features">Features</h3>
-<ul>
-<li>add basic app with model transforms</li>
-</ul>`;const c=["innerHTML"],f={__name:"release-notes",setup(l){return(i,d)=>{const e=s;return o(),t("div",null,[a(e,{id:"release-notes"}),n("div",{id:"version-body",class:"card",innerHTML:r(_)},null,8,c)])}}};export{f as default};
